26112a88c0224a2d30820c3cbeaf1403c4816239
Lights0n <lyadouskiy@gmail.com>
2022-07-01 01:08:58 +0300

    feat: fulfill reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  // console.log(n.toString().reverse);
+  n = n.toString().split('').reverse().join('');
+  if (n.endsWith('-')) {
+    return n.slice(0, n.length - 1)
+  } else {
+    return n
+  }git 
 }
+// console.log(module.exports(-291));
+// console.log(module.exports(252));
+// console.log(module.exports(835));
